c517713d33a01714a79cebef4646d2018b517b12
Ciarán Hanrahan <justciaranhanrahan@gmail.com>
2025-02-18 14:51:24 +1100

    Fix Netlify adapter configuration

--- adapters/netlify/vite.config.ts
+++ adapters/netlify/vite.config.ts
@@ -1,1 +1,22 @@
- 
+import { defineConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import { netlifyEdgeAdapter } from "@builder.io/qwik-city/adapters/netlify-edge/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      netlifyEdgeAdapter(),
+    ],
+    build: {
+      ssr: true,
+      rollupOptions: {
+        input: ["src/entry.netlify.tsx", "@qwik-city-plan"],
+      },
+    },
+  };
+}); 
